804a1431731469bebd5c6b821564c9d6ddde3117
Edward Crompton <edward.crompton@gmail.com>
2021-01-01 20:08:07 +0000

    Initiating develop branch from main

---
+++ web/modules/custom/termau_migrate/src/Plugin/migrate/source/TermauTable.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\termau_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Use a prepopulated table as the source.
+ *
+ * @MigrateSource(
+ *   id = "termau_table"
+ * )
+ */
+class TermauTable extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Source data is queried from 'termau_migrate_porthtermau_data' table.
+    $query = $this->select('termau_migrate_porthtermau_data', 'd')
+      ->fields('d', [
+          'id',
+          'scientific_name',
+          'name_cy',
+          'name_en',
+          'url',
+        ]);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'id' => $this->t('id' ),
+      'scientific_name' => $this->t('scientific_name'),
+      'name_cy' => $this->t('name_cy'),
+      'name_en' => $this->t('name_en'),
+      'url' => $this->t('url'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'id' => [
+        'type' => 'integer',
+        'alias' => 'd',
+      ],
+    ];
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+  }
+}
